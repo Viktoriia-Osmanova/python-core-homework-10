a4cedc216c051836eba44c2df9ca3844aa638c7e
Viktoriia-Osmanova <142603866+Viktoriia-Osmanova@users.noreply.github.com>
2023-09-25 10:28:42 +0200

    HW10

--- main.py
+++ main.py
@@ -1,1 +1,67 @@
 from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if not self.is_valid_phone(value):
+            raise ValueError("Invalid phone number format. Must be 10 digits.")
+        super().__init__(value)
+
+    @staticmethod
+    def is_valid_phone(value):
+        return len(value) == 10 and value.isdigit()
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        if Phone.is_valid_phone(phone):
+            self.phones.append(Phone(phone))
+        else:
+            raise ValueError("Invalid phone number format. Must be 10 digits.")
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        edited = False
+        for phone in self.phones:
+            if phone.value == old_phone:
+                phone.value = new_phone
+                edited = True
+                break
+        if not edited:
+            raise ValueError("Phone number not found.")
+
+    def find_phone(self, phone):
+        found_phones = []
+        for p in self.phones:
+            if p.value == phone:
+                found_phones.append(p.value)
+        return found_phones if found_phones else None
+
+    def __str__(self):
+        phone_str = '; '.join(str(p) for p in self.phones)
+        return f"Contact name: {self.name.value}, phones: {phone_str}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
